64f1e0244dc2146e93585c621c385c5ba0f1640b
ciel <cielchang0324@gmail.com>
2023-05-28 18:18:44 +0800

    basic ok

--- index.js
+++ index.js
@@ -8,70 +8,131 @@
 // $("#answer6").slideUp();
 // });
 
-$(".question.question-1").click(function(){
-  $("#answer1").slideToggle(300);
-  if(document.querySelector(".question-1 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-1 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-1 .question-arrow").textContent="expand_more";
-  }
-});
-$(".question.question-2").click(function(){
-  $("#answer2").slideToggle(300);
-  if(document.querySelector(".question-2 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-2 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-2 .question-arrow").textContent="expand_more";
-  }
-});
-$(".question.question-3").click(function(){
-  $("#answer3").slideToggle(300);
-  if(document.querySelector(".question-3 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-3 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-3 .question-arrow").textContent="expand_more";
-  }
-});
-$(".question.question-4").click(function(){
-  $("#answer4").slideToggle(300);
-  if(document.querySelector(".question-4 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-4 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-4 .question-arrow").textContent="expand_more";
-  }
-});
-$(".question.question-5").click(function(){
-  $("#answer5").slideToggle(300);
-  if(document.querySelector(".question-5 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-5 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-5 .question-arrow").textContent="expand_more";
-  }
-});
-$(".question.question-6").click(function(){
-  $("#answer6").slideToggle(300);
-  if(document.querySelector(".question-6 .question-arrow").textContent=="expand_more"){
-    document.querySelector(".question-6 .question-arrow").textContent="expand_less";
-  }else{
-    document.querySelector(".question-6 .question-arrow").textContent="expand_more";
-  }
-});
+function handleContentReplacement() {
+  $(".question.question-1").click(function () {
+    $("#answer1").slideToggle(300);
+    if (document.querySelector(".question-1 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-1 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-1 .question-arrow").textContent = "expand_more";
+    }
+  });
+  $(".question.question-2").click(function () {
+    $("#answer2").slideToggle(300);
+    if (document.querySelector(".question-2 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-2 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-2 .question-arrow").textContent = "expand_more";
+    }
+  });
+  $(".question.question-3").click(function () {
+    $("#answer3").slideToggle(300);
+    if (document.querySelector(".question-3 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-3 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-3 .question-arrow").textContent = "expand_more";
+    }
+  });
+  $(".question.question-4").click(function () {
+    $("#answer4").slideToggle(300);
+    if (document.querySelector(".question-4 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-4 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-4 .question-arrow").textContent = "expand_more";
+    }
+  });
+  $(".question.question-5").click(function () {
+    $("#answer5").slideToggle(300);
+    if (document.querySelector(".question-5 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-5 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-5 .question-arrow").textContent = "expand_more";
+    }
+  });
+  $(".question.question-6").click(function () {
+    $("#answer6").slideToggle(300);
+    if (document.querySelector(".question-6 .question-arrow").textContent == "expand_more") {
+      document.querySelector(".question-6 .question-arrow").textContent = "expand_less";
+    } else {
+      document.querySelector(".question-6 .question-arrow").textContent = "expand_more";
+    }
+  });
+
+}
 
 
-const ajax_change_area=document.getElementById('ajax-change-area');
+
+const ajax_change_area = document.getElementById('ajax-change-area');
 // console.log(ajax_change_area);
-const area1=document.querySelector('#selector-btn :nth-child(1)');
-const area2=document.querySelector('#selector-btn :nth-child(2)');
-const area3=document.querySelector('#selector-btn :nth-child(3)');
-const area4=document.querySelector('#selector-btn :nth-child(4)');
-console.log(area1,area2,area3,area4);
-area1.addEventListener('click',()=>{
+const area1 = document.querySelector('#selector-btn :nth-child(1)');
+const area2 = document.querySelector('#selector-btn :nth-child(2)');
+const area3 = document.querySelector('#selector-btn :nth-child(3)');
+const area4 = document.querySelector('#selector-btn :nth-child(4)');
+
+
+const btn1 = document.getElementById('btn');
+const changeDiv = document.getElementById('change');
+const allbtn=document.querySelectorAll('#selector-btn a');
+console.log(allbtn);
+
+
+area1.addEventListener('click', (e) => {
+  allbtn.forEach((item,index)=>{
+    item.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  e.preventDefault();
   fetch('./introduce.html')
-    .then(function (response) {
-      return response;
+    .then(response => response.text())
+    .then(data => {
+      ajax_change_area.innerHTML = data;
+      // window.contentReplacementComplete = false;
     })
-    .then(function (data) {
-      console.log(data);
-      console.log('1');
-	})
-})
+});
+
+area2.addEventListener('click', (e) => {
+  allbtn.forEach((item,index)=>{
+    item.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  e.preventDefault();
+  fetch('./questions.html')
+    .then(response => response.text())
+    .then(data => {
+      ajax_change_area.innerHTML = data;
+      window.contentReplacementComplete = true;
+      handleContentReplacement();
+    })
+});
+area3.addEventListener('click', (e) => {
+  allbtn.forEach((item,index)=>{
+    item.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  e.preventDefault();
+  fetch('./progress.html')
+    .then(response => response.text())
+    .then(data => {
+      ajax_change_area.innerHTML = data;
+      // window.contentReplacementComplete = false;
+    })
+});
+area4.addEventListener('click', (e) => {
+  allbtn.forEach((item,index)=>{
+    item.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  e.preventDefault();
+  fetch('./message.html')
+    .then(response => response.text())
+    .then(data => {
+      ajax_change_area.innerHTML = data;
+      // window.contentReplacementComplete = false;
+    })
+});
+
+
+
+
+
+
